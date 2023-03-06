65249ebac0c8b591bffb48d808232bc6838c4781
Jotadebeese <jotadebeese@gmail.com>
2023-03-06 17:03:01 +1100

    Updated files and deleted old ones

---
+++ airbnb-clone/src/components/TopPart.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function TopPart() {
+    return (
+        <section className="hero">
+           <img className="photo-grid" src="./images/photo-grid.png" alt="grid of photos" />
+           <h1>Online Experiences</h1>
+           <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}

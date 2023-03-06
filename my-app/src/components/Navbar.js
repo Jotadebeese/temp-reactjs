c357f423d6e0d5f9125dfff93c18f5d72b2dec7a
Jotadebeese <jotadebeese@gmail.com>
2023-03-06 17:04:36 +1100

    Deleting old files

--- my-app/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <img className="nav--icon" src="./images/airbnb-logo.png" alt="Logo" />
-        </nav>
-    )
-}

d6d56a937ce9b23e5ae1ef1076f9ba8b243dc32d
Jotadebeese <jotadebeese@gmail.com>
2023-03-10 20:14:19 +1100

    small changes

--- MemeGenerator/meme-generator_j/src/Components/Meme.js
+++ MemeGenerator/meme-generator_j/src/Components/Meme.js
@@ -1,9 +1,18 @@
 import React from "react"
 
 export default function Meme() {
+
+    const [imageURL, setImageURL] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setImage(namesArray[randomNumber].url)
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text" 
                     className="form--input"
@@ -15,11 +24,13 @@ export default function Meme() {
                     placeholder="Bottom Text"
                 />
                 <button
+                    onClick={getMemeImage}
                     className="form--button"
                 >
                     Get a new meme image 🌅
                 </button>
-            </form>
+                <img src={imageURL} />
+            </div>
         </main>
         
     )
